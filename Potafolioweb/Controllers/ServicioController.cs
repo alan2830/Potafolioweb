e183d61c0900e5a51561ed3f647e0fd4ad42ba05
Contreras <Gamas Product@DESKTOP-S14JKI4>
2022-12-08 00:55:51 -0800

    Actualizacion de modelo

---
+++ Potafolioweb/Controllers/ServicioController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Potafolioweb.Models;
+
+namespace Potafolioweb.Controllers
+{
+    public class ServicioController : Controller
+    {
+        private portafolioEntities1 db = new portafolioEntities1();
+
+        // GET: Servicios
+        public ActionResult Index()
+        {
+            var servicios = db.Servicios.Include(s => s.AspNetUsers);
+            return View(servicios.ToList());
+        }
+
+        // GET: Servicios/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Servicios servicios = db.Servicios.Find(id);
+            if (servicios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(servicios);
+        }
+
+        // GET: Servicios/Create
+        public ActionResult Create()
+        {
+            ViewBag.UsuarioID = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Servicios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UsuarioID,Descripcion,Servicio,Descripcion_Servicio")] Servicios servicios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Servicios.Add(servicios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UsuarioID = new SelectList(db.AspNetUsers, "Id", "Email", servicios.UsuarioID);
+            return View(servicios);
+        }
+
+        // GET: Servicios/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Servicios servicios = db.Servicios.Find(id);
+            if (servicios == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UsuarioID = new SelectList(db.AspNetUsers, "Id", "Email", servicios.UsuarioID);
+            return View(servicios);
+        }
+
+        // POST: Servicios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UsuarioID,Descripcion,Servicio,Descripcion_Servicio")] Servicios servicios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(servicios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UsuarioID = new SelectList(db.AspNetUsers, "Id", "Email", servicios.UsuarioID);
+            return View(servicios);
+        }
+
+        // GET: Servicios/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Servicios servicios = db.Servicios.Find(id);
+            if (servicios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(servicios);
+        }
+
+        // POST: Servicios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Servicios servicios = db.Servicios.Find(id);
+            db.Servicios.Remove(servicios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
